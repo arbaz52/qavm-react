e0c0f4e883b7b39d6b9f247a9ce495452c9a0c49
arbaz52 <arbaz5256@gmail.com>
2020-09-23 00:04:33 +0500

    first build

---
+++ docs/precache-manifest.f2b8523743184cc1e5966dbfae840743.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ec41d25a99e414ace98622679d7ace6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "de7d104789669b8049f6",
+    "url": "/static/css/2.19e10c4f.chunk.css"
+  },
+  {
+    "revision": "ab4e44b3db8b778766c7",
+    "url": "/static/css/main.6a53a897.chunk.css"
+  },
+  {
+    "revision": "de7d104789669b8049f6",
+    "url": "/static/js/2.254f5b4f.chunk.js"
+  },
+  {
+    "revision": "e0546182649f50a2eb923c2a881ed896",
+    "url": "/static/js/2.254f5b4f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab4e44b3db8b778766c7",
+    "url": "/static/js/main.18f5f4b4.chunk.js"
+  },
+  {
+    "revision": "60120a29a85f9fdf098a",
+    "url": "/static/js/runtime-main.01e326d3.js"
+  }
+]);
